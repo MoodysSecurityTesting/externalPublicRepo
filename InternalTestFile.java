6f1ba34fb9a17b85cc6ed45a8fcafe9c8b7d762a
Rajender Vangala <rajender.vangala@moodys.com>
2026-08-31 10:42:38 -0400

    commit to external repo

--- InternalTestFile.java
+++ InternalTestFile.java
@@ -1,5 +1,7 @@
 public class InternalTestFile {
     public static void main(String[] args) {
         System.out.println("Internal Test File!");
+        System.out.println("Commit 1 to external repo ");
+
     }
 }
