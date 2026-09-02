a9093596a2cd1246faca4d4855344f6c26b3d805
Rajender Vangala <rajender.vangala@moodys.com>
2026-09-02 15:18:14 -0400

    Commit to public repo from VSCode

--- InternalTestFile.java
+++ InternalTestFile.java
@@ -1,7 +1,8 @@
 public class InternalTestFile {
     public static void main(String[] args) {
         System.out.println("Internal Test File!");
-        System.out.println("Commit 1 to external repo - Change made on 0902 - Change made on 0902 - 3:02 pm EST");
+        System.out.println("Commit 1 to external repo - Change made on 0902 - Change made on 0902 - 3:02 pm EST from Browser");
+        System.out.println("Change made on 0902 - 3:16 pm EST from VS Code");
 
     }
 }
